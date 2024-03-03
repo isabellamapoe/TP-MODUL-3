a4bae5bd6fc3a07ffd3058ac1b0b53650b6364d0
isabella <78943065+isabellamapoe@users.noreply.github.com>
2024-03-03 22:28:01 +0700

    commit
    tambahan fitur GUI

---
+++ tpmod3/tpmod3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmod3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            string namaPraktikan = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(namaPraktikan))
+            {
+                label1.Text = "Halo " + namaPraktikan;
+            }
+            else
+            {
+                label1.Text = "Silakan isi nama praktikan terlebih dahulu";
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Submit_Click((object) sender, e);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
